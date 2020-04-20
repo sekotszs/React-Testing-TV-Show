35723ccad9b8deb5d436491e318543cbf5283caa
Zoe Stokes <sekotszs@gmail.com>
2020-04-20 14:59:54 -0700

    more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "./App";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor, fireEvent } from "@testing-library/react";
 import {fetchShow as mockFetchShow} from './api/fetchShow';
 import {showData} from './TestData';
 jest.mock( './api/fetchShow')
@@ -9,6 +9,10 @@ test("App renders", async () => {
     mockFetchShow.mockResolvedValueOnce({
 data: showData
     });
-    const {getByTestId} = render (<App/>)
+    const {getByText} = render (<App/>)
     expect(mockFetchShow).toHaveBeenCalledTimes(1)
-})
+
+    await waitFor (() => getByText(/Select a season/i))
+    fireEvent.mouseDown(getByText(/Select a season/i))
+    await waitFor (() => getByText(/Season 4/i))
+});
